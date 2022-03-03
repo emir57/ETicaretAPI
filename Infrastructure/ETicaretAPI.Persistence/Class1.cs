7981916c16dc3f5fd9a68893ce1d63ed1d69ef18
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-03-03 16:33:37 +0300

    Fix problem

---
+++ Infrastructure/ETicaretAPI.Persistence/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ETicaretAPI.Persistence
+{
+    public class Class1
+    {
+    }
+}
---
+++ Infrastructure/ETicaretAPI.Persistence/Contexts/ETicaretAPIDbContext.cs
@@ -0,0 +1,18 @@
+﻿using ETicaretAPI.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ETicaretAPI.Persistence.Contexts
+{
+    public class ETicaretAPIDbContext : DbContext
+    {
+        public ETicaretAPIDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
