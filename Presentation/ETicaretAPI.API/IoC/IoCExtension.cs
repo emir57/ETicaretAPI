1e67977811ae11ccfe50042ffe37f3ad9494dc18
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-03-03 14:37:17 +0300

    Custom IoC

---
+++ Presentation/ETicaretAPI.API/IoC/IoCExtension.cs
@@ -0,0 +1,20 @@
+﻿using ETicaretAPI.Application.Abstraction;
+using ETicaretAPI.Persistence.Concretes;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.API.IoC
+{
+    public static class IoCExtension
+    {
+        public static IServiceCollection UseCustomIoC(this IServiceCollection services)
+        {
+            services.AddSingleton<IProductService, ProductService>();
+
+            return services;
+        }
+    }
+}
