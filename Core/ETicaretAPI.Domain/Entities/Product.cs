fa020d26142187534f692a820633fea9e388469e
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-03-03 14:17:32 +0300

    Product Entity

--- Core/ETicaretAPI.Domain/Entities/Product.cs
+++ Core/ETicaretAPI.Domain/Entities/Product.cs
@@ -7,5 +7,8 @@ namespace ETicaretAPI.Domain.Entities
 {
     public class Product : BaseEntity
     {
+        public string Name { get; set; }
+        public int Stock { get; set; }
+        public long Price { get; set; }
     }
 }
