2db18f0d92ca7ec05c2086908869a2d1021e714a
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-03-05 14:26:16 +0300

    IRepository,IWriteRepository,IReadRepository

---
+++ Core/ETicaretAPI.Application/Repositories/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ETicaretAPI.Application.Repositories
+{
+    public interface IRepository<T>
+        where T:class
+    {
+        DbSet<T> Table { get; }
+    }
+}
