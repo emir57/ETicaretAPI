1140ff27ea07d3cc52375a30ed21495e0d4470d1
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-03-05 15:37:17 +0300

    Refactoring Name

--- Core/ETicaretAPI.Application/Repositories/IWriteRepository.cs
+++ Core/ETicaretAPI.Application/Repositories/IWriteRepository.cs
@@ -10,7 +10,7 @@ namespace ETicaretAPI.Application.Repositories
         where T:BaseEntity
     {
         Task<bool> AddAsync(T model);
-        Task<bool> AddAsync(List<T> model);
+        Task<bool> AddRangeAsync(List<T> model);
         Task<bool> RemoveAsync(T model);
         Task<bool> RemoveAsync(string id);
         Task<bool> UpdateAsync(T model);
---
+++ Infrastructure/ETicaretAPI.Persistence/Repositories/WriteRepository.cs
@@ -0,0 +1,49 @@
+﻿using ETicaretAPI.Application.Repositories;
+using ETicaretAPI.Domain.Entities.Common;
+using ETicaretAPI.Persistence.Contexts;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Persistence.Repositories
+{
+    public class WriteRepository<TEntity> : IWriteRepository<TEntity>
+        where TEntity : BaseEntity
+    {
+        private readonly ETicaretAPIDbContext _context;
+
+        public WriteRepository(ETicaretAPIDbContext context)
+        {
+            _context = context;
+        }
+
+        public DbSet<TEntity> Table => _context.Set<TEntity>();
+
+        public Task<bool> AddAsync(TEntity model)
+        {
+            
+        }
+
+        public Task<bool> AddRangeAsync(List<TEntity> model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> RemoveAsync(TEntity model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> RemoveAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateAsync(TEntity model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
