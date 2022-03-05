a952d4aae900bf26fe1a2b6dabf9df1a5bae2b92
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-03-05 15:15:08 +0300

    IReadRepository

--- Core/ETicaretAPI.Application/Repositories/IReadRepository.cs
+++ Core/ETicaretAPI.Application/Repositories/IReadRepository.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 
 namespace ETicaretAPI.Application.Repositories
@@ -7,5 +9,9 @@ namespace ETicaretAPI.Application.Repositories
     public interface IReadRepository<T> : IRepository<T>
         where T:class
     {
+        IQueryable<T> GetAll();
+        IQueryable<T> GetWhere(Expression<Func<T,bool>> filter);
+        T GetSingle(Expression<Func<T, bool>> filter);
+        T GetById(string id);
     }
 }
