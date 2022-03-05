199e415c0bfb556cf80e63fe65acea6de603594d
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-03-05 16:05:56 +0300

    Product Concrete Classes

---
+++ Infrastructure/ETicaretAPI.Persistence/Repositories/Product/ProductReadRepository.cs
@@ -0,0 +1,36 @@
+﻿using ETicaretAPI.Application.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Persistence.Repositories.Product
+{
+    public class ProductReadRepository : IProductReadRepository
+    {
+        public DbSet<Domain.Entities.Product> Table => throw new NotImplementedException();
+
+        public IQueryable<Domain.Entities.Product> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Domain.Entities.Product> GetByIdAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Domain.Entities.Product> GetSingleAsync(Expression<Func<Domain.Entities.Product, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<Domain.Entities.Product> GetWhere(Expression<Func<Domain.Entities.Product, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Infrastructure/ETicaretAPI.Persistence/Repositories/Product/ProductWriteRepository.cs
@@ -0,0 +1,47 @@
+﻿using ETicaretAPI.Application.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Persistence.Repositories.Product
+{
+    public class ProductWriteRepository : IProductWriteRepository
+    {
+        public DbSet<Domain.Entities.Product> Table => throw new System.NotImplementedException();
+
+        public Task<bool> AddAsync(Domain.Entities.Product model)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<bool> AddRangeAsync(List<Domain.Entities.Product> models)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool Remove(Domain.Entities.Product model)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<bool> RemoveAsync(string id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool RemoveRange(List<Domain.Entities.Product> models)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<int> SaveAsync()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool Update(Domain.Entities.Product model)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
