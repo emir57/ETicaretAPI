9393171485bdc473406f403a7c4bd8c0b1a69b67
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-03-05 16:03:47 +0300

    Customer Concrete Classes

---
+++ Infrastructure/ETicaretAPI.Persistence/Repositories/Customer/CustomerReadRepository.cs
@@ -0,0 +1,37 @@
+﻿using ETicaretAPI.Application.Repositories;
+using ETicaretAPI.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Persistence.Repositories.Customer
+{
+    public class CustomerReadRepository : ICustomerReadRepository
+    {
+        public DbSet<Customer> Table => throw new NotImplementedException();
+
+        public IQueryable<Customer> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Customer> GetByIdAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Customer> GetSingleAsync(Expression<Func<Customer, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<Customer> GetWhere(Expression<Func<Customer, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Infrastructure/ETicaretAPI.Persistence/Repositories/Customer/CustomerWriteRepository.cs
@@ -0,0 +1,47 @@
+﻿using ETicaretAPI.Application.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Persistence.Repositories.Customer
+{
+    public class CustomerWriteRepository : ICustomerWriteRepository
+    {
+        public DbSet<Domain.Entities.Customer> Table => throw new System.NotImplementedException();
+
+        public Task<bool> AddAsync(Domain.Entities.Customer model)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<bool> AddRangeAsync(List<Domain.Entities.Customer> models)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool Remove(Domain.Entities.Customer model)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<bool> RemoveAsync(string id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool RemoveRange(List<Domain.Entities.Customer> models)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<int> SaveAsync()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool Update(Domain.Entities.Customer model)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
