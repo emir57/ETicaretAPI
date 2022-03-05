c21e5814ec062c976045b4520db5149aaba90919
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-03-05 16:04:50 +0300

    Order Concrete Classes

---
+++ Infrastructure/ETicaretAPI.Persistence/Repositories/Order/OrderReadRepository.cs
@@ -0,0 +1,35 @@
+﻿using ETicaretAPI.Application.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Persistence.Repositories.Order
+{
+    public class OrderReadRepository : IOrderReadRepository
+    {
+        public DbSet<Domain.Entities.Order> Table => throw new NotImplementedException();
+
+        public IQueryable<Domain.Entities.Order> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Domain.Entities.Order> GetByIdAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Domain.Entities.Order> GetSingleAsync(Expression<Func<Domain.Entities.Order, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<Domain.Entities.Order> GetWhere(Expression<Func<Domain.Entities.Order, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Infrastructure/ETicaretAPI.Persistence/Repositories/Order/OrderWriteRepository.cs
@@ -0,0 +1,48 @@
+﻿using ETicaretAPI.Application.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Persistence.Repositories.Order
+{
+    public class OrderWriteRepository : IOrderWriteRepository
+    {
+        public DbSet<Domain.Entities.Order> Table => throw new NotImplementedException();
+
+        public Task<bool> AddAsync(Domain.Entities.Order model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> AddRangeAsync(List<Domain.Entities.Order> models)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(Domain.Entities.Order model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> RemoveAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool RemoveRange(List<Domain.Entities.Order> models)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> SaveAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(Domain.Entities.Order model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
