f65da911a1ddfbb7c95096bfef1dccce5a4e2341
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-03-10 15:34:24 +0300

    SaveChangesAsync interceptor

--- Infrastructure/ETicaretAPI.Persistence/Contexts/ETicaretAPIDbContext.cs
+++ Infrastructure/ETicaretAPI.Persistence/Contexts/ETicaretAPIDbContext.cs
@@ -1,26 +1,38 @@
 ﻿using ETicaretAPI.Domain.Entities;
+using ETicaretAPI.Domain.Entities.Common;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace ETicaretAPI.Persistence.Contexts
 {
     public class ETicaretAPIDbContext : DbContext
     {
-        public ETicaretAPIDbContext(DbContextOptions options) : base(options)
-        {
-        }
         public DbSet<Product> Products { get; set; }
         public DbSet<Order> Orders { get; set; }
         public DbSet<Customer> Customers { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            //modelBuilder.Entity<Product>().Ignore(x => x.Orders);
-            //modelBuilder.Entity<Order>().Ignore(x => x.Products);
             modelBuilder.Entity<OrderProduct>()
                 .HasKey(x => new { x.ProductId, x.OrderId });
             base.OnModelCreating(modelBuilder);
+        }
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            var datas = ChangeTracker.Entries<BaseEntity>();
+            foreach (var data in datas)
+            {
+                var result = data.State switch
+                {
+                    EntityState.Added => data.Entity.CreatedDate = DateTime.Now,
+                    EntityState.Modified => data.Entity.UpdatedDate = DateTime.Now
+                };
+
+            }
+            return base.SaveChangesAsync(cancellationToken);
         }
     }
 }
