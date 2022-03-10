a033340dfe221d2c1c9a596ae7485f841fd60fd7
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-03-10 15:15:21 +0300

    BaseEntity updated date

--- Core/ETicaretAPI.Domain/Entities/Common/BaseEntity.cs
+++ Core/ETicaretAPI.Domain/Entities/Common/BaseEntity.cs
@@ -8,5 +8,7 @@ namespace ETicaretAPI.Domain.Entities.Common
     {
         public Guid Id { get; set; }
         public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
+
     }
 }
