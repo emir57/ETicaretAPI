571925fccefcac5a053d15c443b9e1a6796a300b
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-03-10 15:17:26 +0300

    Add Migrations

---
+++ Infrastructure/ETicaretAPI.Persistence/Migrations/20220310121642_mig_2.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ETicaretAPI.Persistence.Migrations
+{
+    public partial class mig_2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<float>(
+                name: "Price",
+                table: "Products",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedDate",
+                table: "Products",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedDate",
+                table: "Orders",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedDate",
+                table: "Customers",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UpdatedDate",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedDate",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedDate",
+                table: "Customers");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "Price",
+                table: "Products",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(float));
+        }
+    }
+}
