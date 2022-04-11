5fd6644f08627956b63871b4dc6c2a874eda38e5
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-11 16:14:09 +0300

    VM_Update_Product

---
+++ Core/ETicaretAPI.Application/ViewModels/Products/VM_Update_Product.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ETicaretAPI.Application.ViewModels.Products
+{
+    public class VM_Update_Product
+    {
+    }
+}
