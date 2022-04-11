276c5b5b5f087eb29d62e85f5307c8278bb60e89
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-11 16:20:28 +0300

    Deleting product

--- Presentation/ETicaretAPI.API/Controllers/ProductsController.cs
+++ Presentation/ETicaretAPI.API/Controllers/ProductsController.cs
@@ -98,7 +98,9 @@ namespace ETicaretAPI.API.Controllers
         [HttpDelete("delete")]
         public async Task<IActionResult> Delete(string id)
         {
-
+            await _productWriteRepository.RemoveAsync(id);
+            await _productWriteRepository.SaveAsync();
+            return Ok();
         }
     }
 }
