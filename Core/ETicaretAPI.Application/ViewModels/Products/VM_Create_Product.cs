8c373d168397900880a1b57bdcdce6ca776fdead
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-11 16:05:43 +0300

    VM_Create_Product

---
+++ Core/ETicaretAPI.Application/ViewModels/Products/VM_Create_Product.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ETicaretAPI.Application.ViewModels.Products
+{
+    public class VM_Create_Product
+    {
+    }
+}
