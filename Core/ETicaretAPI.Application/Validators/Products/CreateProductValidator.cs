748e16ab8ada0d3f4200c4341efd73090c632a05
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-24 20:49:01 +0300

    CreateProductValidator

---
+++ Core/ETicaretAPI.Application/Validators/Products/CreateProductValidator.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ETicaretAPI.Application.Validators.Products
+{
+    public class CreateProductValidator
+    {
+    }
+}
