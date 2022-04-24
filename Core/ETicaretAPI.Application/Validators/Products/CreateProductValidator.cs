81d1e6e91eb0230cb4495277513e38179b74f055
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-24 20:49:46 +0300

    AbstractValidator

--- Core/ETicaretAPI.Application/Validators/Products/CreateProductValidator.cs
+++ Core/ETicaretAPI.Application/Validators/Products/CreateProductValidator.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using ETicaretAPI.Application.ViewModels.Products;
+using FluentValidation;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace ETicaretAPI.Application.Validators.Products
 {
-    public class CreateProductValidator
+    public class CreateProductValidator : AbstractValidator<VM_Create_Product>
     {
     }
 }
