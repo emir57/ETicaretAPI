d10335b5dc42173752732a57bb8005eed21f1f17
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-24 21:19:48 +0300

    ValidationFilter

---
+++ Infrastructure/ETicaretAPI.Infrastructure/Filters/ValidationFilter.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Infrastructure.Filters
+{
+    public class ValidationFilter : IAsyncActionFilter
+    {
+        public Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
