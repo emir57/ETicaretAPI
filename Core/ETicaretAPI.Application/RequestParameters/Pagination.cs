d2390a65b2058b63bd910444f8f50f3e56666e64
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-04-30 16:06:49 +0300

    Create Struct Pagination

---
+++ Core/ETicaretAPI.Application/RequestParameters/Pagination.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ETicaretAPI.Application.RequestParameters
+{
+    public struct Pagination
+    {
+        public int Page { get; set; }
+        public int Size { get; set; }
+    }
+}
