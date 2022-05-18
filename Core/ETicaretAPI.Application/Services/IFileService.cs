5ec1f8651527a97dcf42184e196c5721dc02c73f
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-05-18 14:48:23 +0300

    IFileService

--- Core/ETicaretAPI.Application/Services/IFileService.cs
+++ Core/ETicaretAPI.Application/Services/IFileService.cs
@@ -1,10 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
 
 namespace ETicaretAPI.Application.Services
 {
     public interface IFileService
     {
+        Task UploadAsync(string path, IFormFileCollection formFiles);
     }
 }
