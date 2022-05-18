6ff7097bffa9a8337be1257a2b5ff56359e2f4da
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-05-18 15:48:38 +0300

    ServiceRegistration and add scope file service

---
+++ Infrastructure/ETicaretAPI.Infrastructure/ServiceRegistration.cs
@@ -0,0 +1,18 @@
+﻿using ETicaretAPI.Application.Services;
+using ETicaretAPI.Infrastructure.Services;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ETicaretAPI.Infrastructure
+{
+    public static class ServiceRegistration
+    {
+        public static void AddInfrastructureService(
+            this IServiceCollection services)
+        {
+            services.AddScoped<IFileService, FileService>();
+        }
+    }
+}
