b8dab309a97efae8bd0fcc58f5f7782bf47001ae
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-05-19 20:14:57 +0300

    InvoiceFileReadRepository

---
+++ Infrastructure/ETicaretAPI.Persistence/Repositories/InvoiceFile/InvoiceFileReadRepository.cs
@@ -0,0 +1,16 @@
+﻿using ETicaretAPI.Application.Repositories;
+using ETicaretAPI.Domain.Entities;
+using ETicaretAPI.Persistence.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ETicaretAPI.Persistence.Repositories
+{
+    public class InvoiceFileReadRepository : ReadRepository<InvoiceFile>, IInvoiceFileReadRepository
+    {
+        public InvoiceFileReadRepository(ETicaretAPIDbContext context) : base(context)
+        {
+        }
+    }
+}
