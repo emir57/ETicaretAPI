60d6ff160ec59f3ffcbc0a37a2192e4ad141734a
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-05-19 20:11:47 +0300

    FileWriteRepository

---
+++ Infrastructure/ETicaretAPI.Persistence/Repositories/File/FileWriteRepository.cs
@@ -0,0 +1,13 @@
+﻿using ETicaretAPI.Application.Repositories;
+using ETicaretAPI.Persistence.Contexts;
+using F = ETicaretAPI.Domain.Entities;
+
+namespace ETicaretAPI.Persistence.Repositories
+{
+    public class FileWriteRepository : WriteRepository<F.File>, IFileWriteRepository
+    {
+        public FileWriteRepository(ETicaretAPIDbContext context) : base(context)
+        {
+        }
+    }
+}
