c2d40d5c3cfcbf9aa11194e2d1ab81c9731fba49
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-05-19 19:49:22 +0300

    context add files entity

--- Infrastructure/ETicaretAPI.Persistence/Contexts/ETicaretAPIDbContext.cs
+++ Infrastructure/ETicaretAPI.Persistence/Contexts/ETicaretAPIDbContext.cs
@@ -18,6 +18,9 @@ namespace ETicaretAPI.Persistence.Contexts
         public DbSet<Product> Products { get; set; }
         public DbSet<Order> Orders { get; set; }
         public DbSet<Customer> Customers { get; set; }
+        public DbSet<Domain.Entities.File> Files { get; set; }
+        public DbSet<ProductImageFile> ProductImageFiles { get; set; }
+        public DbSet<InvoiceFile> InvoiceFiles { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<OrderProduct>()
