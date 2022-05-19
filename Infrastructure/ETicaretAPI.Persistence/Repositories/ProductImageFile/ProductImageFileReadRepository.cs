208e03f743e3b0b4157286920c2dcfae9786b18b
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-05-19 20:13:16 +0300

    ProductImageFileReadRepository

---
+++ Infrastructure/ETicaretAPI.Persistence/Repositories/ProductImageFile/ProductImageFileReadRepository.cs
@@ -0,0 +1,16 @@
+﻿using ETicaretAPI.Application.Repositories;
+using ETicaretAPI.Domain.Entities;
+using ETicaretAPI.Persistence.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ETicaretAPI.Persistence.Repositories
+{
+    public class ProductImageFileReadRepository : ReadRepository<ProductImageFile>, IProductImageFileReadRepository
+    {
+        public ProductImageFileReadRepository(ETicaretAPIDbContext context) : base(context)
+        {
+        }
+    }
+}
