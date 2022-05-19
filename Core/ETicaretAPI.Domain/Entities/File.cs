910b780a93a3013593e2ace59d6bb4105387389e
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-05-19 19:40:54 +0300

    File Entity UpdatedDate [NotMapped]

--- Core/ETicaretAPI.Domain/Entities/File.cs
+++ Core/ETicaretAPI.Domain/Entities/File.cs
@@ -1,12 +1,14 @@
 ﻿using ETicaretAPI.Domain.Entities.Common;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace ETicaretAPI.Domain.Entities
 {
     public class File : BaseEntity
     {
-        
+        [NotMapped]
+        public override DateTime UpdatedDate { get => base.UpdatedDate; set => base.UpdatedDate = value; }
     }
 }
