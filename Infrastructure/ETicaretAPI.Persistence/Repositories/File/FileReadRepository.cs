7cc81174efe55648130eeec5964849eb3f549a6e
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-05-19 20:10:57 +0300

    FileReadRepository

---
+++ Infrastructure/ETicaretAPI.Persistence/Repositories/File/FileReadRepository.cs
@@ -0,0 +1,16 @@
+﻿using ETicaretAPI.Application.Repositories;
+using ETicaretAPI.Persistence.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using F = ETicaretAPI.Domain.Entities;
+
+namespace ETicaretAPI.Persistence.Repositories.File
+{
+    public class FileReadRepository : ReadRepository<F.File>, IFileReadRepository
+    {
+        public FileReadRepository(ETicaretAPIDbContext context) : base(context)
+        {
+        }
+    }
+}
