63f2484daa8b46451663a99995c20dd595fcdf4b
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-05-19 20:07:20 +0300

    IInvoiceFileReadRepository

---
+++ Core/ETicaretAPI.Application/Repositories/InvoiceFile/IInvoiceFileReadRepository.cs
@@ -0,0 +1,11 @@
+﻿using ETicaretAPI.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ETicaretAPI.Application.Repositories
+{
+    public interface IInvoiceFileReadRepository : IReadRepository<InvoiceFile>
+    {
+    }
+}
