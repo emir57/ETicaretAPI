113c543be70ad1323759f6951ad29c4a46f0fd59
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-05-19 19:38:04 +0300

    Create File Entity

---
+++ Core/ETicaretAPI.Domain/Entities/File.cs
@@ -0,0 +1,12 @@
+﻿using ETicaretAPI.Domain.Entities.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ETicaretAPI.Domain.Entities
+{
+    public class File : BaseEntity
+    {
+        
+    }
+}
