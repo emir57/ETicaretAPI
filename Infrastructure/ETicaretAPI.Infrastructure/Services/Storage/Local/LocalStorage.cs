188a0a5697b056cfb8a55c2ea2d4b964c3e3080c
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-05-23 16:31:49 +0300

    LocalStorage => namespace

--- Infrastructure/ETicaretAPI.Infrastructure/Services/Storage/Local/LocalStorage.cs
+++ Infrastructure/ETicaretAPI.Infrastructure/Services/Storage/Local/LocalStorage.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ETicaretAPI.Infrastructure.Services.Local
+namespace ETicaretAPI.Infrastructure.Services.Storage.Local
 {
     public class LocalStorage : ILocalStorage
     {
