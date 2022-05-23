e0ea96774dde943ac12ebb219afae2fbfb6564f8
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-05-23 16:34:08 +0300

    impement interface

--- Infrastructure/ETicaretAPI.Infrastructure/Services/Storage/StorageService.cs
+++ Infrastructure/ETicaretAPI.Infrastructure/Services/Storage/StorageService.cs
@@ -1,11 +1,32 @@
 ﻿using ETicaretAPI.Application.Abstractions.Storage;
+using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace ETicaretAPI.Infrastructure.Services.Storage
 {
     public class StorageService : IStorageService
     {
+        public Task DeleteAsync(string pathOrContainerName, string fileName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<string> GetFiles(string pathOrContainerName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool HasFile(string pathOrContainerName, string fileName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<(string fileName, string pathOrContainerName)>> UploadAsync(string pathOrContainerName, IFormFileCollection formFiles)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
