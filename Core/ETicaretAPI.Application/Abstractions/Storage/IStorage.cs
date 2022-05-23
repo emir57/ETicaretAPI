228cfdd5eccff09f742c9564264b3d1f43efba37
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-05-23 16:07:27 +0300

    UploadAsync

--- Core/ETicaretAPI.Application/Abstractions/Storage/IStorage.cs
+++ Core/ETicaretAPI.Application/Abstractions/Storage/IStorage.cs
@@ -1,10 +1,15 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace ETicaretAPI.Application.Abstractions.Storage
 {
     public interface IStorage
     {
+        Task<List<(string fileName, string pathOrContainer)>> UploadAsync
+            (string pathOrContainer, IFormFileCollection formFiles);
+
     }
 }
