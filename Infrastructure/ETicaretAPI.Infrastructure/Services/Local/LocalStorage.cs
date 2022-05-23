e77378af6f34e391f5fa07b44f2ca2cc4673445f
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-05-23 16:12:06 +0300

    Create LocalStorage

---
+++ Infrastructure/ETicaretAPI.Infrastructure/Services/Local/LocalStorage.cs
@@ -0,0 +1,32 @@
+﻿using ETicaretAPI.Application.Abstractions.Storage.Local;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Infrastructure.Services.Local
+{
+    public class LocalStorage : ILocalStorage
+    {
+        public Task DeleteAsync(string pathOrContainerName, string fileName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<string> GetFiles(string pathOrContainerName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool HasFile(string pathOrContainerName, string fileName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<(string fileName, string pathOrContainerName)>> UploadAsync(string pathOrContainerName, IFormFileCollection formFiles)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
