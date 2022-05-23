5817c1541059d63c4688dc9b9c0d6641ebc6c234
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-05-23 16:16:17 +0300

    LocalStorage

--- Infrastructure/ETicaretAPI.Infrastructure/Services/Local/LocalStorage.cs
+++ Infrastructure/ETicaretAPI.Infrastructure/Services/Local/LocalStorage.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,24 +10,41 @@ namespace ETicaretAPI.Infrastructure.Services.Local
 {
     public class LocalStorage : ILocalStorage
     {
-        public Task DeleteAsync(string pathOrContainerName, string fileName)
+        public Task DeleteAsync(string path, string fileName)
         {
             throw new NotImplementedException();
         }
 
-        public List<string> GetFiles(string pathOrContainerName)
+        public List<string> GetFiles(string path)
         {
             throw new NotImplementedException();
         }
 
-        public bool HasFile(string pathOrContainerName, string fileName)
+        public bool HasFile(string path, string fileName)
         {
             throw new NotImplementedException();
         }
 
-        public Task<List<(string fileName, string pathOrContainerName)>> UploadAsync(string pathOrContainerName, IFormFileCollection formFiles)
+        public async Task<List<(string fileName, string path)>> UploadAsync(string path, IFormFileCollection formFiles)
         {
-            throw new NotImplementedException();
+            string uploadPath = Path.Combine(
+                Directory.GetCurrentDirectory(), "wwwroot",
+                path);
+            if (!File.Exists(uploadPath))
+                Directory.CreateDirectory(uploadPath);
+
+            List<(string fileName, string path)> datas =
+                new List<(string fileName, string path)>();
+            List<bool> results = new List<bool>();
+            foreach (IFormFile file in formFiles)
+            {
+                bool result = await CopyFileAsync(Path.Combine(uploadPath, fileNewName), file);
+                datas.Add((fileNewName, Path.Combine(path, fileNewName)));
+                results.Add(result);
+            }
+            if (results.TrueForAll(r => r.Equals(true)))
+                return datas;
+            return null;
         }
     }
 }
