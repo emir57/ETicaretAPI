7f1700891c2282d73691e72ec088ae9ded395b53
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-05-23 16:33:52 +0300

    Create StorageService

---
+++ Infrastructure/ETicaretAPI.Infrastructure/Services/Storage/StorageService.cs
@@ -0,0 +1,11 @@
+﻿using ETicaretAPI.Application.Abstractions.Storage;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ETicaretAPI.Infrastructure.Services.Storage
+{
+    public class StorageService : IStorageService
+    {
+    }
+}
