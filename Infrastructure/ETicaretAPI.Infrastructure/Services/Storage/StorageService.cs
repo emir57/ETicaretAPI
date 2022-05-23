079ac04b98db4b1751bc00c0688da48e2abb5089
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-05-23 17:07:20 +0300

    StorageName getType name

--- Infrastructure/ETicaretAPI.Infrastructure/Services/Storage/StorageService.cs
+++ Infrastructure/ETicaretAPI.Infrastructure/Services/Storage/StorageService.cs
@@ -15,7 +15,7 @@ namespace ETicaretAPI.Infrastructure.Services.Storage
             _storage = storage;
         }
 
-        public string StorageName { get;}
+        public string StorageName { get => _storage.GetType().Name; }
 
         public async Task DeleteAsync(string pathOrContainerName, string fileName)
             => await _storage.DeleteAsync(pathOrContainerName, fileName);
