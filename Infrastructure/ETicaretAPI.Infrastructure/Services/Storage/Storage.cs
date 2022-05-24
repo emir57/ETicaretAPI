af2c8b5689a4c6b3bb956c20d1940227cb89415b
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-05-24 15:21:03 +0300

    hasFileMethod

--- Infrastructure/ETicaretAPI.Infrastructure/Services/Storage/Storage.cs
+++ Infrastructure/ETicaretAPI.Infrastructure/Services/Storage/Storage.cs
@@ -10,7 +10,7 @@ namespace ETicaretAPI.Infrastructure.Services.Storage
     public class Storage
     {
         protected delegate bool HasFile(string pathOrContainerName, string filePath);
-        protected async Task<string> FileRenameAsync(string path, string fileName, HasFile hasFileMethod, int num = 1)
+        protected async Task<string> FileRenameAsync(string pathOrContainerName, string fileName, HasFile hasFileMethod, int num = 1)
         {
             return await Task.Run(async () =>
             {
@@ -18,9 +18,9 @@ namespace ETicaretAPI.Infrastructure.Services.Storage
                 string oldName = $"{Path.GetFileNameWithoutExtension(fileName)}-{num}";
                 string newFileName = $"{NameOperation.CharacterRegulatory(oldName)}{extension}";
 
-                if (File.Exists($"{path}\\{newFileName}"))
+                if (hasFileMethod(pathOrContainerName, newFileName))
                 {
-                    return await FileRenameAsync(path, $"{newFileName.Split($"-{num}")[0]}{extension}", ++num);
+                    return await FileRenameAsync(pathOrContainerName, $"{newFileName.Split($"-{num}")[0]}{extension}", hasFileMethod, ++num);
                 }
                 return newFileName;
             });
