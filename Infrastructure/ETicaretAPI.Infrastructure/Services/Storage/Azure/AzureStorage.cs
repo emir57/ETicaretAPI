9db9c57dbe3e90ffe6371ed84aab22684a3a9810
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-05-24 14:25:01 +0300

    containerName

--- Infrastructure/ETicaretAPI.Infrastructure/Services/Storage/Azure/AzureStorage.cs
+++ Infrastructure/ETicaretAPI.Infrastructure/Services/Storage/Azure/AzureStorage.cs
@@ -18,22 +18,22 @@ namespace ETicaretAPI.Infrastructure.Services.Storage.Azure
             _blobServiceClient = new BlobServiceClient(configuration["Storage:Azure"]);
         }
 
-        public Task DeleteAsync(string pathOrContainerName, string fileName)
+        public Task DeleteAsync(string containerName, string fileName)
         {
             throw new NotImplementedException();
         }
 
-        public List<string> GetFiles(string pathOrContainerName)
+        public List<string> GetFiles(string containerName)
         {
             throw new NotImplementedException();
         }
 
-        public bool HasFile(string pathOrContainerName, string fileName)
+        public bool HasFile(string containerName, string fileName)
         {
             throw new NotImplementedException();
         }
 
-        public Task<List<(string fileName, string pathOrContainerName)>> UploadAsync(string pathOrContainerName, IFormFileCollection formFiles)
+        public Task<List<(string fileName, string pathOrContainerName)>> UploadAsync(string containerName, IFormFileCollection formFiles)
         {
             throw new NotImplementedException();
         }
