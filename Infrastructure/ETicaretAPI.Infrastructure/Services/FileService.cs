38620bf637da6d4c0e33112a3f12c2d134daefea
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-05-24 15:09:15 +0300

    Storage FileRenameAsync

--- Infrastructure/ETicaretAPI.Infrastructure/Services/FileService.cs
+++ Infrastructure/ETicaretAPI.Infrastructure/Services/FileService.cs
@@ -11,20 +11,6 @@ namespace ETicaretAPI.Infrastructure.Services
 {
     public class FileService
     {
-        private async Task<string> FileRenameAsync(string path, string fileName, int num = 1)
-        {
-            return await Task.Run(async () =>
-            {
-                string extension = Path.GetExtension(fileName);
-                string oldName = $"{Path.GetFileNameWithoutExtension(fileName)}-{num}";
-                string newFileName = $"{NameOperation.CharacterRegulatory(oldName)}{extension}";
-
-                if (File.Exists($"{path}\\{newFileName}"))
-                {
-                    return await FileRenameAsync(path, $"{newFileName.Split($"-{num}")[0]}{extension}", ++num);
-                }
-                return newFileName;
-            });
-        }
+       
     }
 }
---
+++ Infrastructure/ETicaretAPI.Infrastructure/Services/Storage/Storage.cs
@@ -0,0 +1,28 @@
+﻿using ETicaretAPI.Infrastructure.Operations;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Infrastructure.Services.Storage
+{
+    public class Storage
+    {
+        protected async Task<string> FileRenameAsync(string path, string fileName, int num = 1)
+        {
+            return await Task.Run(async () =>
+            {
+                string extension = Path.GetExtension(fileName);
+                string oldName = $"{Path.GetFileNameWithoutExtension(fileName)}-{num}";
+                string newFileName = $"{NameOperation.CharacterRegulatory(oldName)}{extension}";
+
+                if (File.Exists($"{path}\\{newFileName}"))
+                {
+                    return await FileRenameAsync(path, $"{newFileName.Split($"-{num}")[0]}{extension}", ++num);
+                }
+                return newFileName;
+            });
+        }
+    }
+}
