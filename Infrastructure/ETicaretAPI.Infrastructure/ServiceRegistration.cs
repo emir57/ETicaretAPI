a55f3bf835a5539e9563db8a439d6b2b87baa28b
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-05-24 15:11:13 +0300

    AddStorage refactoring where

--- Infrastructure/ETicaretAPI.Infrastructure/ServiceRegistration.cs
+++ Infrastructure/ETicaretAPI.Infrastructure/ServiceRegistration.cs
@@ -20,7 +20,7 @@ namespace ETicaretAPI.Infrastructure
             services.AddScoped<IStorageService, StorageService>();
         }
         public static void AddStorage<T>(this IServiceCollection services)
-            where T : class, IStorage
+            where T : Storage, IStorage
         {
             services.AddScoped<IStorage, T>();
         }
