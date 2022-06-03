fe5e105fe20cdb781ca05651b098c4fbd3e5ec66
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-06-03 18:07:36 +0300

    Refactoring namespaces

---
+++ Core/ETicaretAPI.Application/Features/Commands/ProductImageFile/UploadProductImageFile/UploadProductImageFileCommandHandler.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Application.Features.Commands.ProductImageFile.UploadProductImageFile
+{
+    public class UploadProductImageFileCommandHandler : IRequestHandler<UploadProductImageFileCommandRequest, UploadProductImageFileCommandResponse>
+    {
+        public Task<UploadProductImageFileCommandResponse> Handle(UploadProductImageFileCommandRequest request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Core/ETicaretAPI.Application/Features/Commands/ProductImageFile/UploadProductImageFile/UploadProductImageFileCommandRequest.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ETicaretAPI.Application.Features.Commands.ProductImageFile.UploadProductImageFile
+{
+    public class UploadProductImageFileCommandRequest : IRequest<UploadProductImageFileCommandResponse>
+    {
+    }
+}
