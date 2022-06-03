ef365d14e51ebba44e15288eb8b410f24ee29d8b
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-06-03 14:02:45 +0300

    IRequestHandler

--- Core/ETicaretAPI.Application/Features/Queries/GetAllProduct/GetAllProductQueryHandler.cs
+++ Core/ETicaretAPI.Application/Features/Queries/GetAllProduct/GetAllProductQueryHandler.cs
@@ -1,10 +1,17 @@
-﻿using System;
+﻿using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace ETicaretAPI.Application.Features.Queries.GetAllProduct
 {
-    public class GetAllProductQueryHandler
+    public class GetAllProductQueryHandler : IRequestHandler<GetAllProductQueryRequest, GetAllProductQueryResponse>
     {
+        public Task<GetAllProductQueryResponse> Handle(GetAllProductQueryRequest request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
