faeaebd12a0af1929c1eb870b27d8f81f9a1be4f
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-06-03 14:43:33 +0300

    CreateProductCommandRequest

--- Core/ETicaretAPI.Application/Features/Commands/CreateProduct/CreateProductCommandRequest.cs
+++ Core/ETicaretAPI.Application/Features/Commands/CreateProduct/CreateProductCommandRequest.cs
@@ -8,6 +8,8 @@ namespace ETicaretAPI.Application.Features.Commands.CreateProduct
 {
     public class CreateProductCommandRequest : IRequest<CreateProductCommandResponse>
     {
-        public VM_Create_Product Model { get; set; }
+        public string Name { get; set; }
+        public int Stock { get; set; }
+        public float Price { get; set; }
     }
 }
