b8799e90824d56897c35cae9e492191ad1c99d58
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-06-03 17:49:35 +0300

    UpdateProductCommandHandler

---
+++ Core/ETicaretAPI.Application/Features/Commands/Product/UpdateProduct/UpdateProductCommandHandler.cs
@@ -0,0 +1,25 @@
+﻿using ETicaretAPI.Application.Repositories;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Application.Features.Commands.Product.UpdateProduct
+{
+    public class UpdateProductCommandHandler : IRequestHandler<UpdateProductCommandRequest, UpdateProductCommandResponse>
+    {
+        private readonly IProductWriteRepository _productWriteRepository;
+
+        public UpdateProductCommandHandler(IProductWriteRepository productWriteRepository)
+        {
+            _productWriteRepository = productWriteRepository;
+        }
+
+        public Task<UpdateProductCommandResponse> Handle(UpdateProductCommandRequest request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
