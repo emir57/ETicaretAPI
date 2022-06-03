7761bcc4228311cb1e5ab0c8a3271f1d5912d500
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-06-03 18:06:36 +0300

    UploadProductImageCommandHandler

---
+++ Core/ETicaretAPI.Application/Features/Commands/ProductImageFile/UploadProductImage/UploadProductImageCommandHandler.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Application.Features.Commands.ProductImageFile.UploadProductImage
+{
+    public class UploadProductImageCommandHandler : IRequestHandler<UploadProductImageCommandRequest, UploadProductImageCommandResponse>
+    {
+        public Task<UploadProductImageCommandResponse> Handle(UploadProductImageCommandRequest request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
