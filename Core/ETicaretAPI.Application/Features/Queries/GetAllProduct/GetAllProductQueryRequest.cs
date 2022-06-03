1402d15de89e46b37ba4d2ef5b834f5891bb0bc3
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-06-03 14:01:13 +0300

    IRequest

--- Core/ETicaretAPI.Application/Features/Queries/GetAllProduct/GetAllProductQueryRequest.cs
+++ Core/ETicaretAPI.Application/Features/Queries/GetAllProduct/GetAllProductQueryRequest.cs
@@ -1,11 +1,12 @@
 ﻿using ETicaretAPI.Application.RequestParameters;
+using MediatR;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace ETicaretAPI.Application.Features.Queries.GetAllProduct
 {
-    public class GetAllProductQueryRequest
+    public class GetAllProductQueryRequest : IRequest
     {
         public Pagination Pagination { get; set; }
     }
