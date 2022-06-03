d6e944ee9c00479af1782be90180e8cad0ce4414
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-06-03 17:43:32 +0300

    RemoveProductCommandRequest

---
+++ Core/ETicaretAPI.Application/Features/Commands/Product/RemoveProduct/RemoveProductCommandRequest.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ETicaretAPI.Application.Features.Commands.Product.RemoveProduct
+{
+    public class RemoveProductCommandRequest : IRequest<RemoveProductCommandResponse>
+    {
+        public int Id { get; set; }
+    }
+}
