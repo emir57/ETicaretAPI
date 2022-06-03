199ada81f599ca85340728a450708b2bd118fd60
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-06-03 18:11:30 +0300

    GetProductImagesQueryRequest

---
+++ Core/ETicaretAPI.Application/Features/Queries/ProductImageFile/GetProductImages/GetProductImagesQueryRequest.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ETicaretAPI.Application.Features.Queries.ProductImageFile.GetProductImages
+{
+    public class GetProductImagesQueryRequest : IRequest<GetProductImagesQueryResponse>
+    {
+    }
+}
