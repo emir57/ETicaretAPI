242dc100a4aaf09a82a29a1705a1a88194403027
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-06-03 18:10:49 +0300

    GetProductImagesQueryResponse

---
+++ Core/ETicaretAPI.Application/Features/Queries/ProductImageFile/GetProductImages/GetProductImagesQueryResponse.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ETicaretAPI.Application.Features.Queries.ProductImageFile.GetProductImages
+{
+    public class GetProductImagesQueryResponse
+    {
+    }
+}
