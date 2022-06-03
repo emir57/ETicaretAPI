ed05c401ebab9bc0881e53152f67cded58fc96fb
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-06-03 18:27:10 +0300

    RemoveProductImageFileCommandRequest

---
+++ Core/ETicaretAPI.Application/Features/Commands/ProductImageFile/RemoveProductImageFile/RemoveProductImageFileCommandRequest.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ETicaretAPI.Application.Features.Commands.ProductImageFile.RemoveProductImageFile
+{
+    public class RemoveProductImageFileCommandRequest : IRequest<RemoveProductImageFileCommandResponse>
+    {
+    }
+}
