ca66ca5a32d1656684f77d47b87482ae559502a3
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-06-03 17:19:44 +0300

    GetProductByIdQueryRequest

---
+++ Core/ETicaretAPI.Application/Features/Queries/GetProductById/GetProductByIdQueryRequest.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ETicaretAPI.Application.Features.Queries.GetProductById
+{
+    public class GetProductByIdQueryRequest : IRequest<GetProductByIdQueryResponse>
+    {
+    }
+}
