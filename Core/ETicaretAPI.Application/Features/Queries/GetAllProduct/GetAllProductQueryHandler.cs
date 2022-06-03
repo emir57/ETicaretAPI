dfe825683182ed5ce8ab7cbe27eef32c626c6800
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-06-03 13:52:07 +0300

    Create GetAllProductQueryHandler

---
+++ Core/ETicaretAPI.Application/Features/Queries/GetAllProduct/GetAllProductQueryHandler.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ETicaretAPI.Application.Features.Queries.GetAllProduct
+{
+    public class GetAllProductQueryHandler
+    {
+    }
+}
