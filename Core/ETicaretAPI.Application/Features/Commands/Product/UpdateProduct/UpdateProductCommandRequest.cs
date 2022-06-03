786b1f9666eb3d56dc05bbedca9bb31debcaa9bd
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-06-03 17:48:40 +0300

    UpdateProductCommandRequest

---
+++ Core/ETicaretAPI.Application/Features/Commands/Product/UpdateProduct/UpdateProductCommandRequest.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ETicaretAPI.Application.Features.Commands.Product.UpdateProduct
+{
+    public class UpdateProductCommandRequest : IRequest<UpdateProductCommandResponse>
+    {
+    }
+}
