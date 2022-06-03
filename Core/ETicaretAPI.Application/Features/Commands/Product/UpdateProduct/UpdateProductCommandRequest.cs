6c66ca39181f8ba997f3c7cc51696d89c8c11797
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-06-03 17:50:02 +0300

    UpdateProductCommandRequest add props

--- Core/ETicaretAPI.Application/Features/Commands/Product/UpdateProduct/UpdateProductCommandRequest.cs
+++ Core/ETicaretAPI.Application/Features/Commands/Product/UpdateProduct/UpdateProductCommandRequest.cs
@@ -7,5 +7,9 @@ namespace ETicaretAPI.Application.Features.Commands.Product.UpdateProduct
 {
     public class UpdateProductCommandRequest : IRequest<UpdateProductCommandResponse>
     {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public int Stock { get; set; }
+        public float Price { get; set; }
     }
 }
