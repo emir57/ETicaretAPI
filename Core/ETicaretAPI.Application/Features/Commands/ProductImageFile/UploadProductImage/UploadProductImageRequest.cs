03cac782e931f987e50fdd2c1b0074e942eade25
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-06-03 18:05:05 +0300

    UploadProductImageRequest

---
+++ Core/ETicaretAPI.Application/Features/Commands/ProductImageFile/UploadProductImage/UploadProductImageRequest.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ETicaretAPI.Application.Features.Commands.ProductImageFile.UploadProductImage
+{
+    public class UploadProductImageRequest : IRequest<UploadProductImageCommandResponse>
+    {
+    }
+}
