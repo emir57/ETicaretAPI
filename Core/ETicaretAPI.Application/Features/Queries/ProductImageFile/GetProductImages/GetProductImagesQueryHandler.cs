9f5e78d7fde6707865c2fc944095a206e12828d5
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-06-03 18:12:21 +0300

    GetProductImagesQueryHandler

---
+++ Core/ETicaretAPI.Application/Features/Queries/ProductImageFile/GetProductImages/GetProductImagesQueryHandler.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Application.Features.Queries.ProductImageFile.GetProductImages
+{
+    public class GetProductImagesQueryHandler : IRequestHandler<GetProductImagesQueryRequest, GetProductImagesQueryResponse>
+    {
+        public Task<GetProductImagesQueryResponse> Handle(GetProductImagesQueryRequest request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
