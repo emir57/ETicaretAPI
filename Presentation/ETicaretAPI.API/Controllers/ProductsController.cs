3defccb6fa4a2121e4959ea57e3fd97f911a12f7
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-06-03 17:09:28 +0300

    delete get action

--- Presentation/ETicaretAPI.API/Controllers/ProductsController.cs
+++ Presentation/ETicaretAPI.API/Controllers/ProductsController.cs
@@ -53,43 +53,7 @@ namespace ETicaretAPI.API.Controllers
             _storageService = storageService;
             _mediator = mediator;
         }
-        [HttpGet("get")]
-        public async Task Get()
-        {
-            /*
-            await _productWriteRepository.AddRangeAsync(new List<Domain.Entities.Product>
-            {
-                new Domain.Entities.Product{
-                    Id = Guid.NewGuid(),CreatedDate = DateTime.Now,
-                    Name = "Product 1",
-                    Stock = 5,
-                    Price = (long)299.9},
-                new Domain.Entities.Product
-                {
-                    Id = Guid.NewGuid(),
-                    CreatedDate = DateTime.Now.AddDays(1),
-                    Name = "Product 2",
-                    Stock = 2,
-                    Price = (long)929.9},
-                new Domain.Entities.Product
-                {
-                    Id = Guid.NewGuid(),
-                    CreatedDate = DateTime.Now.AddDays(2),
-                    Name = "Product 3",
-                    Stock = 7,
-                    Price = (long)102.9}
-            });
-            await _productWriteRepository.SaveAsync();
-            */
-            await _productWriteRepository.AddAsync(new Product
-            {
-                Name = "Product 2",
-                Price = 22,
-                Stock = 1,
-                Id = Guid.NewGuid()
-            });
-            await _productWriteRepository.SaveAsync();
-        }
+        
         [HttpGet("getall")]
         public async Task<IActionResult> GetAll([FromQuery] GetAllProductQueryRequest getAllProductQueryRequest)
         {
