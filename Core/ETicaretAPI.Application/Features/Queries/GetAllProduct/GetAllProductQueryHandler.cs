c1f0498e9dc806c2288cd5ed2cba5c51ef9260d3
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-06-03 14:08:11 +0300

    Products change type in response

--- Core/ETicaretAPI.Application/Features/Queries/GetAllProduct/GetAllProductQueryHandler.cs
+++ Core/ETicaretAPI.Application/Features/Queries/GetAllProduct/GetAllProductQueryHandler.cs
@@ -31,7 +31,7 @@ namespace ETicaretAPI.Application.Features.Queries.GetAllProduct
             }).Skip(request.Pagination.Page * request.Pagination.Size).Take(request.Pagination.Size).ToListAsync();
             return new GetAllProductQueryResponse
             {
-
+                TotalCount = totalCount
             };
         }
     }
--- Core/ETicaretAPI.Application/Features/Queries/GetAllProduct/GetAllProductQueryResponse.cs
+++ Core/ETicaretAPI.Application/Features/Queries/GetAllProduct/GetAllProductQueryResponse.cs
@@ -8,6 +8,6 @@ namespace ETicaretAPI.Application.Features.Queries.GetAllProduct
     public class GetAllProductQueryResponse
     {
         public int TotalCount { get; set; }
-        public List<Product> Products { get; set; }
+        public object Products { get; set; }
     }
 }
