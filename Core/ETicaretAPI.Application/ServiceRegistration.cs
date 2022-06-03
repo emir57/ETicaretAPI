93c9dbdb27615228948ff0fb8861604ff011f37c
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-06-03 14:15:05 +0300

    ServiceRegistration AddMediatR

---
+++ Core/ETicaretAPI.Application/ServiceRegistration.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ETicaretAPI.Application
+{
+    public static class ServiceRegistration
+    {
+        public static void AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddMediatR(typeof(ServiceRegistration));
+        }
+    }
+}
