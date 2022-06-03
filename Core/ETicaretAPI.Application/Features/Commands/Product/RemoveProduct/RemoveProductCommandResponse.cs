997f5825afb3472fe61858ba24e5b41df2048c57
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-06-03 17:42:54 +0300

    RemoveProductCommandResponse

---
+++ Core/ETicaretAPI.Application/Features/Commands/Product/RemoveProduct/RemoveProductCommandResponse.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ETicaretAPI.Application.Features.Commands.Product.RemoveProduct
+{
+    public class RemoveProductCommandResponse
+    {
+    }
+}
