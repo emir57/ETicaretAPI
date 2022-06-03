973e545a56e53ff1deb5baebdbf77a0fc2b542e3
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-06-03 17:44:08 +0300

    RemoveProductCommandHandler

---
+++ Core/ETicaretAPI.Application/Features/Commands/Product/RemoveProduct/RemoveProductCommandHandler.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Application.Features.Commands.Product.RemoveProduct
+{
+    public class RemoveProductCommandHandler : IRequestHandler<RemoveProductCommandRequest, RemoveProductCommandResponse>
+    {
+        public Task<RemoveProductCommandResponse> Handle(RemoveProductCommandRequest request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
