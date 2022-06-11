348d7c4d344050234cd0a7469c962ae59372c8da
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-06-11 17:58:46 +0300

    CreateUserCommandRequest add properties

--- Core/ETicaretAPI.Application/Features/Commands/AppUser/CreateUser/CreateUserCommandRequest.cs
+++ Core/ETicaretAPI.Application/Features/Commands/AppUser/CreateUser/CreateUserCommandRequest.cs
@@ -7,5 +7,9 @@ namespace ETicaretAPI.Application.Features.Commands.AppUser.CreateUser
 {
     public class CreateUserCommandRequest : IRequest<CreateUserCommandResponse>
     {
+        public string FirstLastName { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
     }
 }
