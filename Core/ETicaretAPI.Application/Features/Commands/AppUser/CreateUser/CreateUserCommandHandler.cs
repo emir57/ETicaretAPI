9df8f7796adaba1ad15901a3afc74bbdecf29a1d
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-06-11 17:56:00 +0300

    implement interface

--- Core/ETicaretAPI.Application/Features/Commands/AppUser/CreateUser/CreateUserCommandHandler.cs
+++ Core/ETicaretAPI.Application/Features/Commands/AppUser/CreateUser/CreateUserCommandHandler.cs
@@ -2,10 +2,16 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace ETicaretAPI.Application.Features.Commands.AppUser.CreateUser
 {
-    public class CreateUserCommandHandler : IRequestHandler<CreateUserCommandRequest,CreateUserCommandResponse>
+    public class CreateUserCommandHandler : IRequestHandler<CreateUserCommandRequest, CreateUserCommandResponse>
     {
+        public Task<CreateUserCommandResponse> Handle(CreateUserCommandRequest request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
