b25de9ba5e9a83e83f9d380d87cc0575e87dbc18
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-06-11 17:53:43 +0300

    Create CreateUserCommandRequest

---
+++ Core/ETicaretAPI.Application/Features/Commands/AppUser/CreateUser/CreateUserCommandRequest.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ETicaretAPI.Application.Features.Commands.AppUser.CreateUser
+{
+    public class CreateUserCommandRequest
+    {
+    }
+}
