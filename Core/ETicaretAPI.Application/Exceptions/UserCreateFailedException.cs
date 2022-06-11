20b2f054ce6a0655155b0aff221451c697ccdc44
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-06-11 18:15:27 +0300

    Create UserCreateFailedException

---
+++ Core/ETicaretAPI.Application/Exceptions/UserCreateFailedException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace ETicaretAPI.Application.Exceptions
+{
+    public class UserCreateFailedException : Exception
+    {
+        public UserCreateFailedException()
+        {
+
+        }
+
+        public UserCreateFailedException(string message) : base(message)
+        {
+        }
+
+        public UserCreateFailedException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
