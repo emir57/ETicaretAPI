14bcdd3d20f14174149b89285b8966df9a84adae
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-06-11 17:54:59 +0300

    Create CreateUserCommandHandler

---
+++ Core/ETicaretAPI.Application/Features/Commands/AppUser/CreateUser/CreateUserCommandHandler.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ETicaretAPI.Application.Features.Commands.AppUser.CreateUser
+{
+    public class CreateUserCommandHandler : IRequestHandler<CreateUserCommandRequest,CreateUserCommandResponse>
+    {
+    }
+}
