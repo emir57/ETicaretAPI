d56bad741b0b895d336c2aa6d9f1e1ee37124065
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-06-12 14:14:43 +0300

    Create LoginUserCommandResponse

---
+++ Core/ETicaretAPI.Application/Features/Commands/AppUser/LoginUser/LoginUserCommandResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ETicaretAPI.Application.Features.Commands.AppUser.LoginUser
+{
+    public class LoginUserCommandResponse
+    {
+        public bool Succeeded { get; set; }
+        public string Message { get; set; }
+    }
+}
