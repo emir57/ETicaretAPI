f1cfe9d4ea65e5cd1a967f14310bb5cfee0aa31b
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-06-12 16:44:12 +0300

    Create Token

---
+++ Core/ETicaretAPI.Application/Abstractions/Token/ITokenHandler.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ETicaretAPI.Application.Abstractions.Token
+{
+    public interface ITokenHandler
+    {
+        void CreateAccessToken();
+    }
+}
