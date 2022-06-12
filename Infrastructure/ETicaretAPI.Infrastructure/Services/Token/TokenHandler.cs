d71e8faaa9a43d555f69338d02a5a8946219f50d
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-06-12 16:52:28 +0300

    inject TokenOptions and IConfiguration in TokenHandler

--- Infrastructure/ETicaretAPI.Infrastructure/Services/Token/TokenHandler.cs
+++ Infrastructure/ETicaretAPI.Infrastructure/Services/Token/TokenHandler.cs
@@ -1,4 +1,7 @@
 ﻿using ETicaretAPI.Application.Abstractions.Token;
+using ETicaretAPI.Application.Dtos;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -7,9 +10,18 @@ namespace ETicaretAPI.Infrastructure.Services.Token
 {
     public class TokenHandler : ITokenHandler
     {
+        public IConfiguration Configuration { get; }
+        private TokenOptions _tokenOptions;
+        public TokenHandler(IConfiguration configuration)
+        {
+            Configuration = configuration;
+            _tokenOptions = Configuration.GetSection("TokenOptions").Get<TokenOptions>();
+        }
+
         public Application.Dtos.Token CreateAccessToken()
         {
-            throw new NotImplementedException();
+            Application.Dtos.Token token = new Application.Dtos.Token();
+            SymmetricSecurityKey securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_tokenOptions.SecurityKey));
         }
     }
 }
