a41d7a2eb250641f4a3785bf6e750cacf663b6ef
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-06-12 14:44:39 +0300

    Create NotFoundUserException

---
+++ Core/ETicaretAPI.Application/Exceptions/NotFoundUserException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ETicaretAPI.Application.Exceptions
+{
+    public class NotFoundUserException : Exception
+    {
+        public NotFoundUserException() : base("Kullanıcı adı veya şifer hatalı")
+        {
+        }
+
+        public NotFoundUserException(string message) : base(message)
+        {
+        }
+
+        public NotFoundUserException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
