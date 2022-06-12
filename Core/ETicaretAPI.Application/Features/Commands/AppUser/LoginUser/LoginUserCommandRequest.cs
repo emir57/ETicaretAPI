4f9ef1ce98b158cf77a72c1dc00428d3e23bfbfc
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-06-12 14:13:59 +0300

    Create LoginUserCommandRequest

---
+++ Core/ETicaretAPI.Application/Features/Commands/AppUser/LoginUser/LoginUserCommandRequest.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ETicaretAPI.Application.Features.Commands.AppUser.LoginUser
+{
+    public class LoginUserCommandRequest : IRequest<LoginUserCommandResponse>
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
