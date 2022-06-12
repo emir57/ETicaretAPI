667ff0d2d88370b6cd83d03ec28869352c621cee
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-06-12 14:15:32 +0300

    Create LoginUserCommandHandler

---
+++ Core/ETicaretAPI.Application/Features/Commands/AppUser/LoginUser/LoginUserCommandHandler.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Application.Features.Commands.AppUser.LoginUser
+{
+    public class LoginUserCommandHandler : IRequestHandler<LoginUserCommandRequest, LoginUserCommandResponse>
+    {
+        public Task<LoginUserCommandResponse> Handle(LoginUserCommandRequest request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
