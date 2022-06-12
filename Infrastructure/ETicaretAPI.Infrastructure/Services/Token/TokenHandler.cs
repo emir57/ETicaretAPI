1595dadf58d55d9e3dfa83248c318f9fa27b60e7
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-06-12 16:47:32 +0300

    Create TokenHandler

---
+++ Infrastructure/ETicaretAPI.Infrastructure/Services/Token/TokenHandler.cs
@@ -0,0 +1,15 @@
+﻿using ETicaretAPI.Application.Abstractions.Token;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ETicaretAPI.Infrastructure.Services.Token
+{
+    public class TokenHandler : ITokenHandler
+    {
+        public Application.Dtos.Token CreateAccessToken()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
