7cf108b6d08295a8be746cb668ea5e0431b5d72d
emir57 <emir.gurbuz06@hotmail.com>
2022-07-06 15:21:36 +0300

    GoogleLoginCommandHandler

--- Core/ETicaretAPI.Application/Features/Commands/AppUser/GoogleLogin/GoogleLoginCommandHandler.cs
+++ Core/ETicaretAPI.Application/Features/Commands/AppUser/GoogleLogin/GoogleLoginCommandHandler.cs
@@ -1,5 +1,10 @@
-﻿using MediatR;
+﻿using ETicaretAPI.Application.Abstractions.Token;
+using ETicaretAPI.Application.Dtos;
+using Google.Apis.Auth;
+using MediatR;
 using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -8,15 +13,50 @@ namespace ETicaretAPI.Application.Features.Commands.AppUser.GoogleLogin
     public class GoogleLoginCommandHandler : IRequestHandler<GoogleLoginCommandRequest, GoogleLoginCommandResponse>
     {
         private readonly UserManager<Domain.Identity.AppUser> _userManager;
+        private readonly ITokenHandler _tokenHandler;
 
-        public GoogleLoginCommandHandler(UserManager<Domain.Identity.AppUser> userManager)
+        public GoogleLoginCommandHandler(UserManager<Domain.Identity.AppUser> userManager, ITokenHandler tokenHandler)
         {
             _userManager = userManager;
+            _tokenHandler = tokenHandler;
         }
 
-        public Task<GoogleLoginCommandResponse> Handle(GoogleLoginCommandRequest request, CancellationToken cancellationToken)
+        public async Task<GoogleLoginCommandResponse> Handle(GoogleLoginCommandRequest request, CancellationToken cancellationToken)
         {
-            throw new System.NotImplementedException();
+            var settings = new GoogleJsonWebSignature.ValidationSettings()
+            {
+                Audience = new List<string> { "935630960852-j5a1ect8utc4lsdl888d5c63fupctqb4.apps.googleusercontent.com" }
+            };
+            var payload = await GoogleJsonWebSignature.ValidateAsync(request.IdToken, settings);
+
+            var info = new UserLoginInfo(request.Provider, payload.Subject, request.Provider);
+            var appUser = await _userManager.FindByLoginAsync(info.LoginProvider, info.ProviderKey);
+            bool result = appUser != null;
+            if (appUser == null)
+            {
+                appUser = await _userManager.FindByEmailAsync(payload.Email);
+                if (appUser == null)
+                {
+                    appUser = new Domain.Identity.AppUser
+                    {
+                        Id = Guid.NewGuid().ToString(),
+                        Email = payload.Email,
+                        UserName = payload.Email,
+                        NameSurname = payload.Name
+                    };
+                    var identityResult = await _userManager.CreateAsync(appUser);
+                    result = identityResult.Succeeded;
+                }
+            }
+            if (result)
+                await _userManager.AddLoginAsync(appUser, info);
+            else
+                throw new Exception("Invalid external authentication.");
+            Token token = _tokenHandler.CreateAccessToken();
+            return new GoogleLoginCommandResponse
+            {
+                Token = token
+            };
         }
     }
 }
