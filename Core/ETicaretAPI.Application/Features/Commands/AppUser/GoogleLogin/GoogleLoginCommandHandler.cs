ac8331078e41679eb9ed03e736bfa13c0620acc3
emir57 <emir.gurbuz06@hotmail.com>
2022-07-06 15:05:58 +0300

    inject _userManager

--- Core/ETicaretAPI.Application/Features/Commands/AppUser/GoogleLogin/GoogleLoginCommandHandler.cs
+++ Core/ETicaretAPI.Application/Features/Commands/AppUser/GoogleLogin/GoogleLoginCommandHandler.cs
@@ -1,4 +1,5 @@
 ﻿using MediatR;
+using Microsoft.AspNetCore.Identity;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -6,6 +7,13 @@ namespace ETicaretAPI.Application.Features.Commands.AppUser.GoogleLogin
 {
     public class GoogleLoginCommandHandler : IRequestHandler<GoogleLoginCommandRequest, GoogleLoginCommandResponse>
     {
+        private readonly UserManager<Domain.Identity.AppUser> _userManager;
+
+        public GoogleLoginCommandHandler(UserManager<Domain.Identity.AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
         public Task<GoogleLoginCommandResponse> Handle(GoogleLoginCommandRequest request, CancellationToken cancellationToken)
         {
             throw new System.NotImplementedException();
