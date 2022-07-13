9248c38f346a9a5906d90be8ce9d7ace18339907
emir57 <emir.gurbuz06@hotmail.com>
2022-07-13 18:04:46 +0300

    AddHttpClient in startup

--- Core/ETicaretAPI.Application/Features/Commands/AppUser/FacebookLogin/FacebookLoginCommandHandler.cs
+++ Core/ETicaretAPI.Application/Features/Commands/AppUser/FacebookLogin/FacebookLoginCommandHandler.cs
@@ -1,4 +1,8 @@
-﻿using MediatR;
+﻿using ETicaretAPI.Application.Abstractions.Token;
+using Google.Apis.Http;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -6,9 +10,24 @@ namespace ETicaretAPI.Application.Features.Commands.AppUser.FacebookLogin
 {
     public class FacebookLoginCommandHandler : IRequestHandler<FacebookLoginCommandRequest, FacebookLoginCommandResponse>
     {
-        public Task<FacebookLoginCommandResponse> Handle(FacebookLoginCommandRequest request, CancellationToken cancellationToken)
+        private readonly UserManager<Domain.Identity.AppUser> _userManager;
+        private readonly ITokenHandler _tokenHandler;
+        private readonly HttpClient _httpClient;
+
+        public FacebookLoginCommandHandler(UserManager<Domain.Identity.AppUser> userManager, ITokenHandler tokenHandler, IHttpClientFactory httpClientFactory)
         {
-            throw new System.NotImplementedException();
+            _userManager = userManager;
+            _tokenHandler = tokenHandler;
+            _httpClient = httpClientFactory.CreateHttpClient(new CreateHttpClientArgs { });
+        }
+
+        public async Task<FacebookLoginCommandResponse> Handle(FacebookLoginCommandRequest request, CancellationToken cancellationToken)
+        {
+
+            return new FacebookLoginCommandResponse
+            {
+
+            };
         }
     }
 }
--- Presentation/ETicaretAPI.API/Startup.cs
+++ Presentation/ETicaretAPI.API/Startup.cs
@@ -77,6 +77,8 @@ namespace ETicaretAPI.API
                         .WithOrigins("http://localhost:4200");
                 });
             });
+            services.AddHttpClient();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
