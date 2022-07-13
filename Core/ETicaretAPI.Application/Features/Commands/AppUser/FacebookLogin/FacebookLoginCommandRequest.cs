e8748a553fd4b63569f6e05a631b522e4620ce88
emir57 <emir.gurbuz06@hotmail.com>
2022-07-13 17:58:35 +0300

    FacebookLoginCommandRequest add AuthToken Property

--- Core/ETicaretAPI.Application/Features/Commands/AppUser/FacebookLogin/FacebookLoginCommandRequest.cs
+++ Core/ETicaretAPI.Application/Features/Commands/AppUser/FacebookLogin/FacebookLoginCommandRequest.cs
@@ -4,5 +4,6 @@ namespace ETicaretAPI.Application.Features.Commands.AppUser.FacebookLogin
 {
     public class FacebookLoginCommandRequest : IRequest<FacebookLoginCommandResponse>
     {
+        public string AuthToken { get; set; }
     }
 }
