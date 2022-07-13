07816013137a5b09da407223dac2d3d32b873f9f
emir57 <emir.gurbuz06@hotmail.com>
2022-07-14 01:00:52 +0300

    Create FacebookUserInfoResponse

---
+++ Core/ETicaretAPI.Application/Dtos/Facebook/FacebookUserInfoResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace ETicaretAPI.Application.Dtos.Facebook
+{
+    public class FacebookUserInfoResponse
+    {
+        public string Id { get; set; }
+        public string Email { get; set; }
+        public string Name { get; set; }
+    }
+}
