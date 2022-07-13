0981a6bbc1c395c261122f0b47ecaf8f33d3a2d3
emir57 <emir.gurbuz06@hotmail.com>
2022-07-13 18:28:58 +0300

    Create FacebookAccessTokenResponseDto

---
+++ Core/ETicaretAPI.Application/Dtos/Facebook/FacebookAccessTokenResponseDto.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ETicaretAPI.Application.Dtos.Facebook
+{
+    public class FacebookAccessTokenResponseDto
+    {
+        [JsonPropertyName("access_token")]
+        public string AccessToken { get; set; }
+        [JsonPropertyName("token_type")]
+        public string TokenType { get; set; }
+    }
+}
