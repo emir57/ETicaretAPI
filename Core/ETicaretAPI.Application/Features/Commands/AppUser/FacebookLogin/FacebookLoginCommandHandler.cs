b3ad1f6b40f44978a10c253cf8cf0dd7ea67e4f6
emir57 <emir.gurbuz06@hotmail.com>
2022-07-14 01:10:00 +0300

    Complete FacebookLoginCommandHandler

--- Core/ETicaretAPI.Application/Features/Commands/AppUser/FacebookLogin/FacebookLoginCommandHandler.cs
+++ Core/ETicaretAPI.Application/Features/Commands/AppUser/FacebookLogin/FacebookLoginCommandHandler.cs
@@ -1,9 +1,11 @@
 ﻿using ETicaretAPI.Application.Abstractions.Token;
+using ETicaretAPI.Application.Dtos;
 using ETicaretAPI.Application.Dtos.Facebook;
 using Google.Apis.Http;
 using MediatR;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
+using System;
 using System.Net.Http;
 using System.Text.Json;
 using System.Threading;
@@ -45,14 +47,38 @@ namespace ETicaretAPI.Application.Features.Commands.AppUser.FacebookLogin
             {
                 string userInfoResponse = await _httpClient.GetStringAsync(@$"https://graph.facebook.com/ne?
                     fields=email,name&access_token={request.AuthToken}");
-                var facebookUserInfoResponse = JsonSerializer.Deserialize<FacebookUserInfoResponse>(userInfoResponse);
+                var userInfo = JsonSerializer.Deserialize<FacebookUserInfoResponse>(userInfoResponse);
+
+                var info = new UserLoginInfo("FACEBOOK", validation.Data.UserId, "FACEBOOK");
+                var appUser = await _userManager.FindByLoginAsync(info.LoginProvider, info.ProviderKey);
+                bool result = appUser != null;
+                if (appUser == null)
+                {
+                    appUser = await _userManager.FindByEmailAsync(userInfo.Email);
+                    if (appUser == null)
+                    {
+                        appUser = new Domain.Identity.AppUser
+                        {
+                            Id = Guid.NewGuid().ToString(),
+                            Email = userInfo.Email,
+                            UserName = userInfo.Email,
+                            NameSurname = userInfo.Name
+                        };
+                        var identityResult = await _userManager.CreateAsync(appUser);
+                        result = identityResult.Succeeded;
+                    }
+                }
+                if (result)
+                {
+                    await _userManager.AddLoginAsync(appUser, info);
+                    Token token = _tokenHandler.CreateAccessToken();
+                    return new FacebookLoginCommandResponse
+                    {
+                        Token = token
+                    };
+                }
             }
-            
-
-            return new FacebookLoginCommandResponse
-            {
-
-            };
+            throw new Exception("Invalid external authentication.");
         }
     }
 }
