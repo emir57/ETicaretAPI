a7dbb7884e4505b2bec65e0c75a8211403401764
emir57 <emir.gurbuz06@hotmail.com>
2022-07-14 14:36:40 +0300

    Create IAuthService and IUserService

---
+++ Core/ETicaretAPI.Application/Abstractions/Services/IAuthService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ETicaretAPI.Application.Abstractions.Services
+{
+    public interface IAuthService
+    {
+    }
+}
