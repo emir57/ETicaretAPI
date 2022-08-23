db4ca871f10f92f2566533e8d9d6f1657f59d16f
emir57 <emir.gurbuz06@hotmail.com>
2022-08-24 01:50:34 +0300

    Add Seq

--- Presentation/ETicaretAPI.API/Program.cs
+++ Presentation/ETicaretAPI.API/Program.cs
@@ -29,6 +29,7 @@ namespace ETicaretAPI.API
                         .WriteTo.MySQL(
                         "Server=localhost;Port=3306;Database=ETicaretAPIDb;Uid=root;Pwd=123456;",
                         "logs")
+                        .WriteTo.Seq("http://localhost:5341/")
                         .Enrich.FromLogContext()
                         .CreateLogger();
                     webBuilder.UseSerilog(log);
